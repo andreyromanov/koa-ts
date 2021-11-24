fdb3a384ecc9150f5b867c369d4cb2d66830f922
andreyromanov <andreygorogogo@gmail.com>
2021-11-24 20:37:13 +0200

    first commit

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import * as Koa from 'koa';
+import {DefaultState, DefaultContext, ParameterizedContext} from 'koa';
+import * as Router from 'koa-router';
+
+
+const port = 3000;
+const app: Koa<DefaultState, DefaultContext> = new Koa();
+
+const router: Router = new Router();
+
+router.get('/', async (ctx: ParameterizedContext<DefaultContext, DefaultState>, next) => {
+    await next();
+    console.log(ctx);
+    
+    ctx.body = {msg : 'Greetings traveler!'}
+});
+router.get('/', async (ctx, next) => {
+    const start = Date.now();
+    console.log('before', ctx.body);
+    await next();
+    console.log('after', ctx.body);
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+router.get('/', async (ctx) =>{
+    ctx.body = 'Hello Andrii'
+});
+
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () =>{
+    console.log('App started...');
+})
