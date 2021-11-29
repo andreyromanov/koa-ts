733f87c263217296721c0295cf2f29a50e940d8d
Andrii Romanov <andreygorogogo@gmail.com>
2021-11-29 17:52:28 +0200

    feature -> sentury logs

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ import connectDatabase from "./util"
 //import middleware from './middlewares'
 config()
 
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
+
 const app: Koa<DefaultState, DefaultContext> = new Koa();
 
 //app.use(middleware())
@@ -19,6 +23,32 @@ function main (): void {
             console.log('App started...');
             connectDatabase(process.env.MONGO_URL)
         })
+
+        Sentry.init({
+            dsn: "https://a680d02d43344bacbe4162f9e105046d@o1080304.ingest.sentry.io/6085951",
+          
+            // Set tracesSampleRate to 1.0 to capture 100%
+            // of transactions for performance monitoring.
+            // We recommend adjusting this value in production
+            tracesSampleRate: 1.0,
+          });
+          
+          const transaction = Sentry.startTransaction({
+            op: "test",
+            name: "My First Test Transaction",
+          });
+          
+          setTimeout(() => {
+            try {
+                throw new Error('AAaaaaa')
+            } catch (e) {
+              Sentry.captureException(e);
+            } finally {
+              transaction.finish();
+            }
+          }, 99);
+
+
     } catch (error) {
       console.error(error)
     }
